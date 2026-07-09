446fd2c33039e7730b17f0cc69ce0bf567110be1
PED-afk <arpi.nagy07@gmail.com>
2026-07-09 19:03:46 +0200

    Update bot version to 0.6.2

--- discord_deadlock_bot.py
+++ discord_deadlock_bot.py
@@ -1292,7 +1292,7 @@ async def tick():
 bot.startTimers={"A":11*60,"B":11*60}
 bot.timers={"A":{"time":None,"paused":False},"B":{"time":None,"paused":False}}
 bot.bootTime=time.time()//1
-bot.version="0.6.1.cogs"
+bot.version="0.6.2"
 
 
 bot.messageCD=60*60*0.1 #6 minutes
