b9ff6a4388613766b216ca5c946533527ad899d9
PED-afk <arpi.nagy07@gmail.com>
2026-07-09 17:40:18 +0200

    Moved timer control commands to cog
    Removed timer control commands (pause, unpause, start, end, settimer, gettimer, remaining) from the bot.

--- discord_deadlock_bot.py
+++ discord_deadlock_bot.py
@@ -450,6 +450,8 @@ intents.voice_states = True
 class MyBot(commands.Bot):
     async def setup_hook(self):
         await self.load_extension("cogs.hiddens")
+        await self.load_extension("cogs.timer")
+
 #bot=commands.Bot(command_prefix='!', intents=intents)
 bot=MyBot(command_prefix='!', intents=intents)
 
@@ -749,66 +751,6 @@ async def minigames(ctx, game:str=None):
                 await ctx.reply("Get back to the base!\nYou have: "+str(bot.user_data[str(senderID)]["money"]["unsecured"])+" unsecured souls!", view=view)
         else:
             await ctx.reply("No minigame exists with that name."+chooseFaceFromCategory(bot,"nervous"))
-
-@bot.command()
-async def pause(ctx):
-    senderID=ctx.author.id
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel to be able to pause a timer.")
-            else:
-                if bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]!=None and bot.timers[ctx.author.voice.channel.category.name[-2]]["paused"]==False:
-                    bot.timers[ctx.author.voice.channel.category.name[-2]]["paused"]=True
-                    await ctx.reply("Paused timer for the ["+ctx.author.voice.channel.category.name[-2]+"] category.")
-                else:
-                    await ctx.reply("There isn't an active timer in this voice channel category or it's already paused.")
-        else:
-            await ctx.reply("You don't have permission! >:)",delete_after=10)
-                    
-@bot.command()
-async def unpause(ctx):
-    senderID=ctx.author.id
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel to be able to unpause a timer.")
-            else:
-                if bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]!=None and bot.timers[ctx.author.voice.channel.category.name[-2]]["paused"]==True:
-                    bot.timers[ctx.author.voice.channel.category.name[-2]]["paused"]=False
-                    await ctx.reply("Unpaused timer for the ["+ctx.author.voice.channel.category.name[-2]+"] category.")
-                else:
-                    await ctx.reply("There isn't an active timer in this voice channel category or it's already running.")
-        else:
-            await ctx.reply("You don't have permission! >:)",delete_after=10)
-
-@bot.command()
-async def start(ctx):
-    senderID=ctx.author.id
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel to be able to start a timer.")
-            else:
-                if bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]==None:
-                    bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]=time.time()+bot.startTimers[ctx.author.voice.channel.category.name[-2]]
-                    await ctx.reply("Started timer for the ["+ctx.author.voice.channel.category.name[-2]+"] category.")
-                    
-                    name=ctx.author.voice.channel.name[-2]
-                    names=[]
-                    for guild in bot.guilds:
-                        for channel in discord.utils.get(guild.categories, name="["+name+"]").voice_channels:
-                            for member in channel.members:
-                                if member.global_name=="PurpleEarthDragon":
-                                    names.append(member.global_name+chooseFaceFromCategory(bot,"love"))
-                                else:
-                                    names.append(member.global_name)
-                    
-                    await ctx.send("__Good luck, and Have fun!__\n"+'\n'.join(names)+"\n"+chooseFaceFromCategory(bot,"happy"),delete_after=bot.startTimers[ctx.author.voice.channel.category.name[-2]])
-                else:
-                    await ctx.reply("There is already an active timer in this voice channel category.")
-        else:
-            await ctx.reply("You don't have permission! >:)",delete_after=10)
         
 @bot.command()
 async def shutdown(ctx):
@@ -867,51 +809,6 @@ async def sleep(ctx,save:str="save"):
                 
                 await ctx.reply("Going to sleep\nI will be unavailable between "+pauseStart+" and "+pauseEnd+" CEST\n"+chooseFaceFromCategory(bot,"sleep"),delete_after=20)
                 await bot.close()
-        
-@bot.command()
-async def end(ctx):
-    senderID=ctx.author.id
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel so I know which timer to end.")
-            else:
-                if bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]>time.time()-1:
-                    bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]=time.time()-1
-                    await ctx.reply("Timer stoped.")
-
-@bot.command()
-async def endit(ctx):
-    senderID=ctx.author.id
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel so I know which timer to end.")
-            else:
-                if bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]!=None:
-                    bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]=None
-                    await ctx.reply("Timer stoped. Moving noone.")
-
-@bot.command()
-async def settimer(ctx,x:float):
-    senderID=ctx.author.id
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel to change a timer lenght.")
-            else:
-                bot.startTimers[ctx.author.voice.channel.category.name[-2]]=x*60
-                await ctx.reply("Starting time set to "+str(x)+" minutes.")
-            
-@bot.command()
-async def gettimer(ctx):
-    senderID=ctx.author.id
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel to view a timer lenght.")
-            else:
-                await ctx.reply("The timer is set to "+str(bot.startTimers[ctx.author.voice.channel.category.name[-2]]/60)+" minutes.")
 
 @bot.command()
 async def bot_help(ctx, section:str=None):
@@ -994,19 +891,6 @@ async def bot_help(ctx, section:str=None):
 async def ping(ctx):
     if ctx.channel.id==BOTS_CHANNEL_ID:
         await ctx.reply("Pong!",delete_after=2)
-
-@bot.command()
-async def remaining(ctx):
-    if ctx.channel.id==BOTS_CHANNEL_ID:
-        senderID=ctx.author.id
-        if senderID==ME or any(role.id == BOT_ROLE for role in ctx.author.roles):
-            if ctx.author.voice==None:
-                await ctx.reply("You must be in a voice channel to view a timer.")
-            else:
-                if bot.startTimers[ctx.author.voice.channel.category.name[-2]]!=None:
-                    await ctx.reply("Remaining time: "+str(round(abs(bot.timers[ctx.author.voice.channel.category.name[-2]]["time"]-time.time())/60,2))+" min(s).")
-                else:
-                    await ctx.reply("Timer is not active.")
 
 @bot.command()
 async def status(ctx):
