956b0d5326ce0bd3fc303a801913cfb948523fd7
PED-afk <arpi.nagy07@gmail.com>
2026-07-15 20:10:46 +0200

    power cog fix

--- discord_deadlock_bot.py
+++ discord_deadlock_bot.py
@@ -443,7 +443,7 @@ class MyBot(commands.Bot):
         await self.load_extension("cogs.hiddens")
         await self.load_extension("cogs.timer")
         await self.load_extension("cogs.power")
-        await self.load_extension("cogs.priority_cog")
+        #await self.load_extension("cogs.priority_cog")
 
 #bot=commands.Bot(command_prefix='!', intents=intents)
 bot=MyBot(command_prefix='!', intents=intents)
