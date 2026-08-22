2908c8f2a8f216ccfc6a5ff20b2b36d7bb9b33e6
PED-afk <arpi.nagy07@gmail.com>
2026-08-22 14:31:10 +0200

    Update interaction logic to use hidden user data
    Refactor interaction handling to use hidden data structure for user interactions.

--- user_bot_interaction.py
+++ user_bot_interaction.py
@@ -5,43 +5,56 @@ import time
 
 
 from constants import BOT_INTERACTION_TIMEOUT
+from debug import printLog
 
 def checkPastInteractions(data):
   newTimes={}
   for type in data:
     times=[]
-    for time in type:
-      if int(time)+BOT_INTERACTION_TIMEOUT>time.time():
-        times.append(time)
+    for iTime in data[type]:
+      if int(data[type][iTime])+BOT_INTERACTION_TIMEOUT>time.time():
+        times.append(data[type][iTime])
     if len(times)>0:
       newTimes[type]=times
   return newTimes
 
 def interact(bot:commands.Bot,value:float,type:str,id:int):
   userData=bot.user_data
-  userData[id]=checkPastInteractions(userData[id])
-  if type not in userData[id].keys():
-    userData[id][type]=[]
-  userData[id][type].append({str(time.time()):value})
-  userData["global"][type].append({str(time.time()):value/4})
+  id=str(id)
+  userData[id]["hidden"]["interact"]=checkPastInteractions(userData[id]["hidden"]["interact"])
+  userData["global"]["hidden"]["interact"]=checkPastInteractions(userData["global"]["hidden"]["interact"])
+
+  if type not in userData[id]["hidden"]["interact"].keys():
+    userData[id]["hidden"]["interact"][type]={}
+  userData[id]["hidden"]["interact"][type][str(time.time())]=value
+
+  if type not in userData["global"]["hidden"]["interact"].keys():
+    userData["global"]["hidden"]["interact"][type]={}
+  userData["global"]["hidden"]["interact"][type][str(time.time())]=value/4
+
   bot.user_data=userData
 
 def getInteractValue(bot:commands.Bot,type:str,id:int):
   userData=bot.user_data
-  userData[id]=checkPastInteractions(userData[id])
+  id=str(id)
+  userData[id]["hidden"]["interact"]=checkPastInteractions(userData[id]["hidden"]["interact"])
   bot.user_data=userData
+  if type not in userData[id]["hidden"]["interact"].keys():
+    return 0
   value=0
-  for i in userData[id][type]:
-    value+=userData[id][type][i]
+  for i in userData[id]["hidden"]["interact"][type]:
+    value+=userData[id]["hidden"]["interact"][type][i]
   return value
 
 def getGlobalInteractValue(bot:commands.Bot,type:str):
   userData=bot.user_data
-  userData["global"]=checkPastInteractions(userData["global"])
+  userData["global"]["hidden"]["interact"]=checkPastInteractions(userData["global"]["hidden"]["interact"])
   bot.user_data=userData
+  if type not in userData["global"]["hidden"]["interact"].keys():
+    return 0
   value=0
-  for i in userData["global"][type]:
-    value+=userData["global"][type][i]
+  for i in userData["global"]["hidden"]["interact"][type]:
+    value+=userData["global"]["hidden"]["interact"][type][i]
   return value
 
 
